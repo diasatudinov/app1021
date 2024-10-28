c0aaf01da4451bc053a1846eecf6d4407c7ee60f
Dias <dias.atudinov@gmail.com>
2024-10-28 18:24:40 +0500

    add tab view

--- app1021/app1021/First/TabView/TabUIView.swift
+++ app1021/app1021/First/TabView/TabUIView.swift
@@ -8,8 +8,91 @@
 import SwiftUI
 
 struct TabUIView: View {
+    @State var selectedTab = 0
+    private let tabs = ["Home", "Attractions", "Memories"]
+    
+//    @ObservedObject var collectionVM = CollectionViewModel()
+//    @ObservedObject var settingsVM = SettingsViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            switch selectedTab {
+            case 0:
+                Text("1")
+                //HomeUIView(viewModel: collectionVM)
+            case 1:
+                Text("2")
+                //MyCollectionUIView(viewModel: collectionVM)
+            case 2:
+                Text("3")
+                //PlannedUIView(viewModel: collectionVM)
+            default:
+                Text("default")
+            }
+                VStack {
+                    Spacer()
+                    
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.secondaryBg)
+                            .frame(height: 83)
+                            
+                        HStack(spacing: 100) {
+                            ForEach(0..<tabs.count) { index in
+                                Button(action: {
+                                    selectedTab = index
+                                }) {
+                                    
+                                    ZStack {
+                                        VStack {
+                                            if index == 0 {
+                                                Image(icon(for: index))
+                                                    .resizable()
+                                                    .renderingMode(.template)
+                                                    .scaledToFit()
+                                                    .frame(height: 28)
+                                                    .foregroundColor(selectedTab == index ? Color.yellowBtn : Color.white.opacity(0.7))
+                                                    .font(.system(size: 20, weight: .semibold))
+                                                    .padding(.bottom, 2)
+                                            } else {
+                                                Image(systemName: icon(for: index))
+                                                    .resizable()
+                                                    .scaledToFit()
+                                                    .frame(height: 28)
+                                                    .foregroundColor(selectedTab == index ? Color.yellowBtn : Color.white.opacity(0.7))
+                                                    .font(.system(size: 20, weight: .semibold))
+                                                    .padding(.bottom, 2)
+                                            }
+                                        }
+                                    }
+                                }
+                                
+                            }
+                        }.padding(.bottom, 5)
+                    
+                    }
+                    
+                }.ignoresSafeArea()
+            
+        }
+    }
+    
+    private func icon(for index: Int) -> String {
+        switch index {
+        case 0: return "tab1"
+        case 1: return "book.and.wrench.fill"
+        case 2: return "map.fill"
+        default: return ""
+        }
+    }
+    
+    private func text(for index: Int) -> String {
+        switch index {
+        case 0: return "Home"
+        case 1: return "My collection"
+        case 2: return "Planned"
+        default: return ""
+        }
     }
 }
 
