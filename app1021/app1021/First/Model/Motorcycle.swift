70b6fbbdce220a4e5e24800aa9b7eebc76fafd3c
Dias <dias.atudinov@gmail.com>
2024-10-30 20:42:59 +0500

    Moto model

--- app1021/app1021/First/Model/Motorcycle.swift
+++ app1021/app1021/First/Model/Motorcycle.swift
@@ -5,4 +5,25 @@
 //  Created by Dias Atudinov on 30.10.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct Motorcycle {
+    
+    var imageData: Data?
+    var brand: String
+    var model: String
+    var year: Int
+    var mileage: Int
+    var engine: String
+    var cylinders: Int
+    var volume: String
+    var image: UIImage? {
+        get {
+            guard let data = imageData else { return nil }
+            return UIImage(data: data)
+        }
+        set {
+            imageData = newValue?.jpegData(compressionQuality: 1.0)
+        }
+    }
+}
