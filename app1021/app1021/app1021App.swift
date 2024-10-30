39961ca58e8117ed17027fd6d557e5823ed04da9
Dias <dias.atudinov@gmail.com>
2024-10-30 20:42:17 +0500

    Navigation View

--- app1021/app1021/app1021App.swift
+++ app1021/app1021/app1021App.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct app1021App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                LoaderView()
+            }
         }
     }
 }
