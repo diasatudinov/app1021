9a55021add0006a7d3b990e34182278e2ebb0dba
Dias <dias.atudinov@gmail.com>
2024-10-31 18:13:05 +0500

    Contact model

--- app1021/app1021/First/Model/Contact.swift
+++ app1021/app1021/First/Model/Contact.swift
@@ -5,4 +5,13 @@
 //  Created by Dias Atudinov on 31.10.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct Contact: Hashable, Codable {
+    
+    var id = UUID()
+    var name: String
+    var employeeName: String
+    var number: String
+    
+}
